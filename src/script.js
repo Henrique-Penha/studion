8fc049daf4c2ced130f3c2480a7a1ba2c9a5dc7d
Henrique Penha <henrique-dsp34@hotmail.com>
2023-04-13 15:51:01 -0300

    feat(adjusting): @media screen.

--- src/script.js
+++ src/script.js
@@ -1,18 +1,27 @@
 $(document).ready(() => {
-    const slickOptions = {
+  // MENU 
+  $('.navbar__menu-btn').on('click', function (){
+    $('.navbar__links').toggleClass('active');
+    $(this).find('i').toggleClass('fa-bars');
+    $(this).find('i').toggleClass('fa-times');
+  })
+
+  // CAROUSEL
+  const slickOptions = {
       autoplay: true,
       dots: false,
       prevArrow:
         '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
       nextArrow:
         '<button type="button" class="slick-next slider__next-arrow">Next</button>;'
-    };
-    const counterOptions = {
+  };
+
+  const counterOptions = {
       delay: 20,
       time: 2000
-    };
-    $('.slider').slick(slickOptions);
-    $('.counter__number').counterUp(counterOptions);
+  };
+  $('.slider').slick(slickOptions);
+  $('.counter__number').counterUp(counterOptions);
 })
 
 $(document).ready(() => {
